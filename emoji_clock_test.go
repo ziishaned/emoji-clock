fb6a66bc41a49295720dc5596221803c9e1f43e9
Zeeshan Ahmad <ziishaned@gmail.com>
2018-10-08 13:08:14 +0400

    Add tests for TimeToEmoji

--- emoji_clock_test.go
+++ emoji_clock_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestTimeToEmojiShouldReturnString(t *testing.T) {
+	var utc bool
+	currentTime := time.Now()
+	emoji := TimeToEmoji(currentTime, utc)
+	if reflect.TypeOf(emoji).String() != "string" {
+		t.Errorf("Expected type was string but got %s", reflect.TypeOf(emoji).String())
+	}
+}
