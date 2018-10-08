dc8f1431e8251a0f60cc71d2c1d45d7522603bc3
Zeeshan Ahmad <ziishaned@gmail.com>
2018-10-08 13:27:24 +0400

    Format code

--- emoji_clock.go
+++ emoji_clock.go
@@ -1,65 +1,65 @@
 package main
 
 import (
-	"time"
+  "time"
 )
 
 var clock = map[float64]string{
-	0:    "🕛",
-	1:    "🕐",
-	2:    "🕑",
-	3:    "🕒",
-	4:    "🕓",
-	5:    "🕔",
-	6:    "🕕",
-	7:    "🕖",
-	8:    "🕗",
-	9:    "🕘",
-	10:   "🕙",
-	11:   "🕚",
-	0.5:  "🕧",
-	1.5:  "🕜",
-	2.5:  "🕝",
-	3.5:  "🕞",
-	4.5:  "🕟",
-	5.5:  "🕠",
-	6.5:  "🕡",
-	7.5:  "🕢",
-	8.5:  "🕣",
-	9.5:  "🕤",
-	10.5: "🕥",
-	11.5: "🕦",
+  0:    "🕛",
+  1:    "🕐",
+  2:    "🕑",
+  3:    "🕒",
+  4:    "🕓",
+  5:    "🕔",
+  6:    "🕕",
+  7:    "🕖",
+  8:    "🕗",
+  9:    "🕘",
+  10:   "🕙",
+  11:   "🕚",
+  0.5:  "🕧",
+  1.5:  "🕜",
+  2.5:  "🕝",
+  3.5:  "🕞",
+  4.5:  "🕟",
+  5.5:  "🕠",
+  6.5:  "🕡",
+  7.5:  "🕢",
+  8.5:  "🕣",
+  9.5:  "🕤",
+  10.5: "🕥",
+  11.5: "🕦",
 }
 
 func hoursAndMinutesToEmoji(hours float64, minutes float64) string {
-	if hours > 11 {
-		hours = hours - 12
-	}
-	minutes = minutes / 60
-	if minutes < 0.25 {
-		minutes = 0
-	} else if minutes >= 0.25 && minutes < 0.75 {
-		minutes = 0.5
-	} else {
-		if hours == 11 {
-			hours = 0
-		} else {
-			hours = hours + 1
-		}
-		minutes = 0
-	}
-	return clock[float64(hours)+minutes]
+  if hours > 11 {
+    hours = hours - 12
+  }
+  minutes = minutes / 60
+  if minutes < 0.25 {
+    minutes = 0
+  } else if minutes >= 0.25 && minutes < 0.75 {
+    minutes = 0.5
+  } else {
+    if hours == 11 {
+      hours = 0
+    } else {
+      hours = hours + 1
+    }
+    minutes = 0
+  }
+  return clock[float64(hours)+minutes]
 }
 
 func TimeToEmoji(time time.Time, utc bool) string {
-	var hours float64
-	var minutes float64
-	if utc == true {
-		hours = float64(time.UTC().Hour())
-		minutes = float64(time.UTC().Minute())
-	} else {
-		hours = float64(time.Hour())
-		minutes = float64(time.Minute())
-	}
-	return hoursAndMinutesToEmoji(hours, minutes)
+  var hours float64
+  var minutes float64
+  if utc == true {
+    hours = float64(time.UTC().Hour())
+    minutes = float64(time.UTC().Minute())
+  } else {
+    hours = float64(time.Hour())
+    minutes = float64(time.Minute())
+  }
+  return hoursAndMinutesToEmoji(hours, minutes)
 }
--- emoji_clock_test.go
+++ emoji_clock_test.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"reflect"
-	"testing"
-	"time"
+  "fmt"
+  "reflect"
+  "testing"
+  "time"
 )
 
 func TestItShouldReturnString(t *testing.T) {
-	currentTime := time.Now()
-	emoji := TimeToEmoji(currentTime, false)
-	if reflect.TypeOf(emoji).String() != "string" {
-		t.Errorf("Expected type was string but got %s", reflect.TypeOf(emoji).String())
-	}
+  currentTime := time.Now()
+  emoji := TimeToEmoji(currentTime, false)
+  if reflect.TypeOf(emoji).String() != "string" {
+    t.Errorf("Expected type was string but got %s", reflect.TypeOf(emoji).String())
+  }
 }
 
 func TestItShouldReturnTheCorrectEmoji(t *testing.T) {
-	timeStr := "2014-11-12T11:45:26.371Z"
-	time2, err := time.Parse("2006-01-02T15:04:05.000Z", timeStr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	emoji := TimeToEmoji(time2, true)
-	if emoji != "🕛" {
-		t.Errorf("Expected emoji is 🕛 but received %s", emoji)
-	}
+  timeStr := "2014-11-12T11:45:26.371Z"
+  time2, err := time.Parse("2006-01-02T15:04:05.000Z", timeStr)
+  if err != nil {
+    fmt.Println(err)
+  }
+  emoji := TimeToEmoji(time2, true)
+  if emoji != "🕛" {
+    t.Errorf("Expected emoji is 🕛 but received %s", emoji)
+  }
 }
